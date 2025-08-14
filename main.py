ed4531abf350cd6d2400c4ed92506f22efeeafe2
gumike128 <147023617+gumike128@users.noreply.github.com>
2025-08-14 14:46:16 +0200

    Slovenská lokalizácia main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.environ['GROQ_API_KEY']
-#GROQ_API_KEY = os.environ.get("GROQ_API_KEY", None)
 
 if 'api_key' not in st.session_state:
     st.session_state.api_key = GROQ_API_KEY
@@ -23,33 +22,24 @@ class GenerationStatistics:
         self.output_time = output_time
         self.input_tokens = input_tokens
         self.output_tokens = output_tokens
-        self.total_time = total_time # Sum of queue, prompt (input), and completion (output) times
+        self.total_time = total_time
         self.model_name = model_name
 
     def get_input_speed(self):
-        """ 
-        Tokens per second calculation for input
-        """
         if self.input_time != 0:
             return self.input_tokens / self.input_time
         else:
             return 0
     
     def get_output_speed(self):
-        """ 
-        Tokens per second calculation for output
-        """
         if self.output_time != 0:
             return self.output_tokens / self.output_time
         else:
             return 0
     
     def add(self, other):
-        """
-        Add statistics from another GenerationStatistics object to this one.
-        """
         if not isinstance(other, GenerationStatistics):
-            raise TypeError("Can only add GenerationStatistics objects")
+            raise TypeError("Možno pridať iba objekty typu GenerationStatistics")
         
         self.input_time += other.input_time
         self.output_time += other.output_time
@@ -58,12 +48,12 @@ class GenerationStatistics:
         self.total_time += other.total_time
 
     def __str__(self):
-        return (f"\n## {self.get_output_speed():.2f} T/s ⚡\nRound trip time: {self.total_time:.2f}s  Model: {self.model_name}\n\n"
-                f"| Metric          | Input          | Output          | Total          |\n"
-                f"|-----------------|----------------|-----------------|----------------|\n"
-                f"| Speed (T/s)     | {self.get_input_speed():.2f}            | {self.get_output_speed():.2f}            | {(self.input_tokens + self.output_tokens) / self.total_time if self.total_time != 0 else 0:.2f}            |\n"
-                f"| Tokens          | {self.input_tokens}            | {self.output_tokens}            | {self.input_tokens + self.output_tokens}            |\n"
-                f"| Inference Time (s) | {self.input_time:.2f}            | {self.output_time:.2f}            | {self.total_time:.2f}            |")
+        return (f"\n## {self.get_output_speed():.2f} T/s ⚡\nCelkový čas generovania: {self.total_time:.2f}s  Model: {self.model_name}\n\n"
+                f"| Metrika          | Vstup          | Výstup          | Celkom          |\n"
+                f"|------------------|----------------|-----------------|-----------------|\n"
+                f"| Rýchlosť (T/s)   | {self.get_input_speed():.2f}           | {self.get_output_speed():.2f}           | {(self.input_tokens + self.output_tokens) / self.total_time if self.total_time != 0 else 0:.2f}           |\n"
+                f"| Tokeny           | {self.input_tokens}           | {self.output_tokens}           | {self.input_tokens + self.output_tokens}           |\n"
+                f"| Čas inferencie (s)| {self.input_time:.2f}           | {self.output_time:.2f}           | {self.total_time:.2f}           |")
 
 class Book:
     def __init__(self, structure):
@@ -71,7 +61,7 @@ class Book:
         self.contents = {title: "" for title in self.flatten_structure(structure)}
         self.placeholders = {title: st.empty() for title in self.flatten_structure(structure)}
 
-        st.markdown("## Generating the following:")
+        st.markdown("## Generujem nasledujúcu štruktúru knihy:")
         toc_columns = st.columns(4)
         self.display_toc(self.structure, toc_columns)
         st.markdown("---")
@@ -88,7 +78,7 @@ class Book:
         try:
             self.contents[title] += new_content
             self.display_content(title)
-        except TypeError as e:
+        except TypeError:
             pass
 
     def display_content(self, title):
@@ -100,7 +90,7 @@ class Book:
             structure = self.structure
         
         for title, content in structure.items():
-            if self.contents[title].strip():  # Only display title if there is content
+            if self.contents[title].strip():
                 st.markdown(f"{'#' * level} {title}")
                 self.placeholders[title].markdown(self.contents[title])
             if isinstance(content, dict):
@@ -116,43 +106,34 @@ class Book:
         return col_index
 
     def get_markdown_content(self, structure=None, level=1):
-        """
-        Returns the markdown styled pure string with the contents.
-        """
         if structure is None:
             structure = self.structure
         
         markdown_content = ""
         for title, content in structure.items():
-            if self.contents[title].strip():  # Only include title if there is content
+            if self.contents[title].strip():
                 markdown_content += f"{'#' * level} {title}\n{self.contents[title]}\n\n"
             if isinstance(content, dict):
                 markdown_content += self.get_markdown_content(content, level + 1)
         return markdown_content
 
 def create_markdown_file(content: str) -> BytesIO:
-    """
-    Create a Markdown file from the provided content.
-    """
     markdown_file = BytesIO()
     markdown_file.write(content.encode('utf-8'))
     markdown_file.seek(0)
     return markdown_file
 
 def generate_book_structure(prompt: str):
-    """
-    Returns book structure content as well as total tokens and total time for generation.
-    """
     completion = st.session_state.groq.chat.completions.create(
         model="llama3-70b-8192",
         messages=[
             {
                 "role": "system",
-                "content": "Write in JSON format:\n\n{\"Title of section goes here\":\"Description of section goes here\",\n\"Title of section goes here\":{\"Title of section goes here\":\"Description of section goes here\",\"Title of section goes here\":\"Description of section goes here\",\"Title of section goes here\":\"Description of section goes here\"}}"
+                "content": "Write in JSON format:\n\n{\"Názov sekcie\":\"Popis sekcie\",\n\"Názov sekcie\":{\"Názov podsekcie\":\"Popis podsekcie\"}}"
             },
             {
                 "role": "user",
-                "content": f"Write a comprehensive structure, omiting introduction and conclusion sections (forward, author's note, summary), for a long (>300 page) book on the following subject:\n\n<subject>{prompt}</subject>"
+                "content": f"Napíš komplexnú štruktúru knihy bez úvodu a záveru (predhovor, poznámka autora, zhrnutie) pre rozsiahlu (>300 strán) knihu na tému:\n\n<subject>{prompt}</subject>\nGeneruj štruktúru v slovenčine."
             }
         ],
         temperature=0.3,
@@ -174,11 +155,11 @@ def generate_section(prompt: str):
         messages=[
             {
                 "role": "system",
-                "content": "You are an expert writer. Generate a long, comprehensive, structured chapter for the section provided."
+                "content": "Si expert na písanie kníh. Vygeneruj dlhú, komplexnú, štruktúrovanú kapitolu pre danú sekciu. Generuj obsah v slovenčine."
             },
             {
                 "role": "user",
-                "content": f"Generate a long, comprehensive, structured chapter for the following section:\n\n<section_title>{prompt}</section_title>"
+                "content": f"Vygeneruj dlhú, komplexnú, štruktúrovanú kapitolu pre nasledujúcu sekciu:\n\n<section_title>{prompt}</section_title>\nGeneruj obsah v slovenčine."
             }
         ],
         temperature=0.3,
@@ -199,20 +180,19 @@ def generate_section(prompt: str):
             statistics_to_return = GenerationStatistics(input_time=usage.prompt_time, output_time=usage.completion_time, input_tokens=usage.prompt_tokens, output_tokens=usage.completion_tokens, total_time=usage.total_time,model_name="llama3-8b-8192")
             yield statistics_to_return
 
-# Initialize
 if 'button_disabled' not in st.session_state:
     st.session_state.button_disabled = False
 
 if 'button_text' not in st.session_state:
-    st.session_state.button_text = "Generate"
+    st.session_state.button_text = "Generovať knihu"
 
 if 'statistics_text' not in st.session_state:
     st.session_state.statistics_text = ""
 
-
 st.write("""
-# Groqbook: Write full books using llama3 (8b and 70b) on Groq
-""")
+# Groqbook: Generuj celé knihy v slovenčine pomocou Llama3 (8b a 70b) na Groq
+"""
+)
 
 def disable():
     st.session_state.button_disabled = True
@@ -224,34 +204,29 @@ def empty_st():
     st.empty()
 
 try:
-    if st.button('End Generation and Download Book'):
+    if st.button('Ukonči generovanie a stiahni knihu'):
         if "book" in st.session_state:
             markdown_file = create_markdown_file(st.session_state.book.get_markdown_content())
             st.download_button(
-                label='Confirm Download',
+                label='Potvrdiť stiahnutie',
                 data=markdown_file,
-                file_name='generated_book.txt',
+                file_name='vygenerovana_kniha.txt',
                 mime='text/plain'
             )
         else:
-            raise ValueError("Please generate content first before downloading the book.")
+            raise ValueError("Najskôr vygeneruj obsah knihy, až potom môžeš stiahnuť súbor.")
 
     with st.form("groqform"):
-        #if not GROQ_API_KEY:
-        #    groq_input_key = st.text_input("Enter your Groq API Key (gsk_yA...):", "",type="password")
+        topic_text = st.text_input("O čom má byť kniha?", "")
 
-        topic_text = st.text_input("What do you want the book to be about?", "")
-
-        # Generate button
         submitted = st.form_submit_button(st.session_state.button_text,on_click=disable,disabled=st.session_state.button_disabled)
         
-        # Statistics
         placeholder = st.empty()
         def display_statistics():
             with placeholder.container():
                 if st.session_state.statistics_text:
-                    if "Generating structure in background" not in st.session_state.statistics_text:
-                        st.markdown(st.session_state.statistics_text+"\n\n---\n") # Format with line if showing statistics
+                    if "Generujem štruktúru na pozadí" not in st.session_state.statistics_text:
+                        st.markdown(st.session_state.statistics_text+"\n\n---\n")
                     else:
                         st.markdown(st.session_state.statistics_text)
                 else:
@@ -259,20 +234,16 @@ try:
 
         if submitted:
             if len(topic_text)<10:
-                raise ValueError("Book topic must be at least 10 characters long")
+                raise ValueError("Téma knihy musí mať aspoň 10 znakov.")
 
             st.session_state.button_disabled = True
-            # st.write("Generating structure in background....")
-            st.session_state.statistics_text = "Generating structure in background...." # Show temporary message before structure is generated and statistics show
+            st.session_state.statistics_text = "Generujem štruktúru na pozadí...."
             display_statistics()
 
             if not GROQ_API_KEY:
                 st.session_state.groq = Groq(api_key=groq_input_key)
 
             large_model_generation_statistics, book_structure = generate_book_structure(topic_text)
-
-            # st.session_state.statistics_text = str(large_model_generation_statistics)
-            # display_statistics()
 
             total_generation_statistics = GenerationStatistics(model_name="llama3-8b-8192")
 
@@ -283,9 +254,7 @@ try:
                 if 'book' not in st.session_state:
                     st.session_state.book = book
 
-                # Print the book structure to the terminal to show structure
                 print(json.dumps(book_structure_json, indent=2))
-
                 st.session_state.book.display_structure()
 
                 def stream_section_content(sections):
@@ -293,14 +262,11 @@ try:
                         if isinstance(content, str):
                             content_stream = generate_section(title+": "+content)
                             for chunk in content_stream:
-                                # Check if GenerationStatistics data is returned instead of str tokens
                                 chunk_data = chunk
                                 if (type(chunk_data)==GenerationStatistics):
                                     total_generation_statistics.add(chunk_data)
-                                    
                                     st.session_state.statistics_text = str(total_generation_statistics)
                                     display_statistics()
-
                                 elif chunk!=None:
                                     st.session_state.book.update_content(title, chunk)
                         elif isinstance(content, dict):
@@ -309,7 +275,7 @@ try:
                 stream_section_content(book_structure_json)
             
             except json.JSONDecodeError:
-                st.error("Failed to decode the book structure. Please try again.")
+                st.error("Nepodarilo sa dekódovať štruktúru knihy. Skús to znova.")
 
             enable()
 
@@ -317,5 +283,5 @@ except Exception as e:
     st.session_state.button_disabled = False
     st.error(e)
 
-    if st.button("Clear"):
+    if st.button("Vyčistiť"):
         st.rerun()
